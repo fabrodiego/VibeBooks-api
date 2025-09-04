e8c8b84e18bddc532011380284e41205c5c5733b
diego. <7ogeid@users.noreply.github.com>
2025-09-03 22:54:17 -0300

    Origin

---
+++ src/main/java/com/vibebooks/api/VibeBooksApplication.java
@@ -0,0 +1,13 @@
+package com.vibebooks.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VibeBooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vibebooks/api/VibeBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibebooks.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
