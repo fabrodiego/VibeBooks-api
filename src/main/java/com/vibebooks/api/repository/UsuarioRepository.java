23c9723d242ead74676c1a813c564e143434598a
diego. <7ogeid@users.noreply.github.com>
2025-09-16 18:51:21 -0300

    Criação interface repository UsuarioRepository

---
+++ src/main/java/com/vibebooks/api/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.vibebooks.api.repository;
+
+import com.vibebooks.api.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+}
