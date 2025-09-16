b7a63f05d4f0c591418f521472f82f1cbe65fe4b
diego. <7ogeid@users.noreply.github.com>
2025-09-16 18:50:44 -0300

    Criação interface repository CurtidaRepository

--- src/main/java/com/vibebooks/api/repository/CurtidaRepository.java
+++ src/main/java/com/vibebooks/api/repository/CurtidaRepository.java
@@ -1,9 +1,12 @@
 package com.vibebooks.api.repository;
 
-import com.vibebooks.api.model.Usuarios;
+import com.vibebooks.api.model.Curtida;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
+public interface CurtidaRepository extends JpaRepository<Curtida, UUID> {
+
 }
